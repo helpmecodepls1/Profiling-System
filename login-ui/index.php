e777c90a7206ad4b8c9abb0f5296b7e9fe557b9d
Lance Ylyza <lancemagollado@gmail.com>
2024-11-21 14:36:14 +0800

    Hello,love

---
+++ login-ui/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>E-STRANDYANTE</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" type="x-icon" href="login-ui/images/icons/Logo.png"/>
+	<link rel="stylesheet" type="text/css" href="login-ui/vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/css/util.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/css/main.css">
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-form-title" style="background-image: url(login-ui/images/sanbartolome.jpg);">
+					<span class="login100-form-title-1">
+					K-12 Program Student<br> 
+					Profiling System
+					</span>
+					<span class="login100-form-title-2">
+					"Empowering student success<br> 
+                     through data-driven insights"
+					</span>
+					<span class="login100-form-title-3">
+					© 2024 [Quezon City University/District V].<br> 
+                    All rights reserved. Version 1.0.1
+					</span>
+				</div>
+
+				<form method="post" id="examineeLoginFrm" class="login100-form validate-form">
+				<div>
+	<img src="login-ui/images/icons/Logo-removebg-preview.png" class="form-logo" alt="E-Strandyante">
+</div>
+					<div class="wrap-input100 validate-input m-b-26 wrap1" data-validate="Username is required">
+						
+						<!-- <span class="label-input100">Email</span> -->
+						<input class="input100" type="text" name="username" placeholder="Enter email">
+						<span class="focus-input100"></span>
+					
+					</div>
+
+	<div class="wrap-input100 validate-input m-b-18" data-validate = "Password is required">
+						<!-- <span class="label-input100">Password</span> -->
+						<input class="input100 password-input" type="password" name="pass" placeholder="Enter password">
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<button type="submit" class="login100-form-btn">
+							Login
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	<script src="login-ui/vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="login-ui/vendor/animsition/js/animsition.min.js"></script>
+	<script src="login-ui/vendor/bootstrap/js/popper.js"></script>
+	<script src="login-ui/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="login-ui/vendor/select2/select2.min.js"></script>
+	<script src="login-ui/vendor/daterangepicker/moment.min.js"></script>
+	<script src="login-ui/vendor/daterangepicker/daterangepicker.js"></script>
+	<script src="login-ui/vendor/countdowntime/countdowntime.js"></script>
+	<script src="login-ui/js/main.js"></script>
+
+</body>
+</html>
